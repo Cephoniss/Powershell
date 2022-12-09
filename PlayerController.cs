fcd888559a4dfbdfb0d295283ab5d6627f381457
Edgardo D Acosta <eacostaiii@gmail.com>
2022-12-09 13:01:52 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,56 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 10.0f;
-    private bool isGrounded = false;
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-    }
-}
-
-This script allows the player to move horizontally using the A and D keys or the left and right arrow keys, and to jump using the Space key. The player's movement is controlled by a Rigidbody2D component, and the script checks for collisions with an object tagged "Ground" to determine when the player is able to jump. You can adjust the speed and jumpForce variables to change the player's movement and jumping behavior.
-Ed A
-Please take the above script and add a double jump method
-
-Sure, here is a modified version of the script that adds a double jump method:
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    public float speed = 5.0f;
-    public float jumpForce = 10.0f;
     public float doubleJumpForce = 5.0f;
     private bool isGrounded = false;
     private bool hasDoubleJumped = false;
